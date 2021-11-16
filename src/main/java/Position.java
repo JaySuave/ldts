f6f2e0b69a5237d34611faf7b80f761a343da240
JaySuave <up202004823@up.pt>
2021-11-16 20:40:00 +0000

    position implemented

---
+++ src/main/java/Position.java
@@ -0,0 +1,24 @@
+public class Position {
+    int x,y;
+    public Position(int i,int j){
+        x=i;
+        y=j;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
